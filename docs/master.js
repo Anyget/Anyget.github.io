939c0ebe274c54134fe74ceac84b709f7e1f66d2
qwegat <63029208+qwegat@users.noreply.github.com>
2021-07-11 00:15:18 +0900

    nbspころした

--- docs/master.js
+++ docs/master.js
@@ -725,7 +725,6 @@ function unescapeHtml(str) {
     let div = document.createElement("div");
     div.innerHTML = str.replace(/</g,"&lt;")
                         .replace(/>/g,"&gt;")
-                        .replace(/ /g, "&nbsp;")
                         .replace(/\r/g, "&#13;")
                         .replace(/\n/g, "&#10;");
     return div.textContent || div.innerText;
